700c1cb04eb02f8819c33bf379e23426ebc660b3
Armin Esmaeilzadeh <lineyno@yahoo.com>
2025-03-01 22:41:32 +0330

    Add All Files And [gitignoreFile.]

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#cccccc',
+      },
+    },
+    FontFamily: {
+      vazirmatn: ['vazirmatn'],
+    },
+  },
+  plugins: [],
+};
